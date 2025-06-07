079c43d1d39c2e0e95043e4d155961ff41d5346c
Kalpana <nemopal50@gmail.com>
2025-06-07 22:31:14 +0530

    day8

---
+++ day8.py
@@ -0,0 +1,9 @@
+#Calculator by Python
+
+a = 56
+b = 3
+
+print("The value of ", a, "+", b, "is : ", a + b)
+print("The value of ", a, "-", b, "is : ", a - b)
+print("The value of ", a, "*", b, "is : ", a * b)
+print("The value of ", a, "/", b, "is : ", a / b)
