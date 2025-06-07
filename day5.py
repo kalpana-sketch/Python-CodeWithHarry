6b9188b69c92cb00d2b34f4285d5c12a3da14771
Kalpana <nemopal50@gmail.com>
2025-06-07 13:09:27 +0530

    day5

---
+++ day5.py
@@ -0,0 +1,17 @@
+"""
+Hey Kalpana, Please don't remove this line
+Author: Kalpana
+Course: 100DaysOfCode 
+"""
+
+print("Hey I am \'a \"good boy\" \nand this viewer is also a good boy/girl")
+
+print("Hello World")
+
+#This is a 'songle-line Comment'
+print("This is a print Statement")
+
+print("Hello world !!!") #Printing Hello world
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Harry")
