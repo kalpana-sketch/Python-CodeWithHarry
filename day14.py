854731387a137f2d91cf1e12a8147392dc9b412c
Kalpana <nemopal50@gmail.com>
2025-06-15 21:30:49 +0530

    day14

---
+++ day14.py
@@ -0,0 +1,30 @@
+# if-else statements:
+applePrice = 210
+budget = 200
+if(applePrice <= budget):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+
+# elif statements:
+num = 0
+if(num < 0):
+    print("Number is negative.")
+elif(num == 0):
+    print("Number is Zero.")
+else:
+    print("Number is positive.")
+
+# nested if statements:
+num = 18
+if(num < 0):
+    print("Number is negative.")
+elif(num > 0):
+    if(num <= 10):
+        print("Number is between 1-10")
+    elif(num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
