27ba2fe0c607f75d519f59104434145e049c00bd
Kalpana <nemopal50@gmail.com>
2025-06-15 22:24:34 +0530

    day16

---
+++ day16.py
@@ -0,0 +1,14 @@
+x = 4
+
+match x:
+    case 0:
+        print("x is zero")
+    # case with if-condition
+    case 4 if x % 2 == 0:
+        print("x % 2 == 0 and case is 4")
+    # Empty case with if-condition
+    case _ if x < 10:
+        print("x is < 10")
+    # default case 
+    case _ :
+        print(x)
